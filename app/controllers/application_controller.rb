a1ba59902b40da0c2b2b336366bdd2f00061e541
Dave Harms <daveharmswebdev@gmail.com>
2022-08-28 11:16:10 -0500

    modified gem file for heroku push

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,8 @@ class ApplicationController < ActionController::Base
   def hello
     render html: "hello world from the app controller!"
   end
+
+  def goodbye
+    render html: "goodbye!"
+  end
 end
